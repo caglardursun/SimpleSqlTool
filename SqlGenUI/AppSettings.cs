c1f96ace76fde509a01ce9917d2076fb152c5b04
Çağlar Dursun <caglardursun@yandex.com>
2020-04-12 22:29:27 +0300

    settings implimented

--- SqlGenUI/AppSettings.cs
+++ SqlGenUI/AppSettings.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,15 +11,72 @@ namespace SqlGenUI
 {
     public class AppSettings
     {
+        [JsonIgnore]
+        private static AppSettings instance = null;
+        [JsonIgnore]
+        public static AppSettings Instance 
+        { 
+            get {
+                if (instance == null)
+                    instance = new AppSettings();
+
+                return instance; 
+            } 
+        }
+        [JsonIgnore]
+        private const string appName = "SqlGenUI";
+        [JsonIgnore]
+        private const string settingFileName = "Settings.json";
+        [JsonIgnore]
+        private static string fileSettingsPath { get; set; }
+
+        [JsonProperty("Path")]
         public string APIPath { get; set; }
 
+        [JsonProperty("Server")]
         public string ServerName { get; set; }
 
+        [JsonProperty("DB")]
         public string DefaultDB { get; set; }
 
+        [JsonProperty("UserName")]
         public string UserName { get; set; }
 
+        [JsonProperty("Password")]
         public string Password { get; set; }
 
+
+        
+        AppSettings()
+        {
+            string folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), appName);
+            if (!Directory.Exists(folderPath))
+                Directory.CreateDirectory(folderPath);
+
+            fileSettingsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), appName, settingFileName);
+
+            if (File.Exists(fileSettingsPath))
+            {
+                var settings = JsonConvert.DeserializeObject<AppSettings>(fileSettingsPath);
+                
+
+                APIPath = settings.APIPath;
+                DefaultDB = settings.DefaultDB;
+                Password = settings.Password;
+                UserName = settings.UserName;
+                ServerName = settings.ServerName;
+            }
+        }
+
+        
+        public void Save()
+        {
+            string output = JsonConvert.SerializeObject(instance);
+            using (StreamWriter sw = new StreamWriter(fileSettingsPath))
+            {
+                sw.Write(output);
+            }
+
+        }
     }
 }
--- SqlGenUI/SettingsForm.cs
+++ SqlGenUI/SettingsForm.cs
@@ -22,61 +22,49 @@ namespace SqlGenUI
     }
 
     public partial class SettingsForm : Form
-    {
-        private AppSettings appSettings;
-        private const string appName = "SqlGenUI";
-        private const string settingFileName = "Settings.json";
-        private string fileSettingsPath;
+    {                
 
         public SettingsForm()
         {
             InitializeComponent();
-            appSettings = new AppSettings();
+            //appSettings = new AppSettings();
             Initialize();
         }
 
         private void Initialize()
         {
-            string folderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), appName);
-            if(!Directory.Exists(folderPath))            
-                Directory.CreateDirectory(folderPath);
-            fileSettingsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), appName, "Settings.json");
+         
 
+            var settings = AppSettings.Instance;
+            textBoxApiPath.Text = settings.APIPath.EmptyCheck();
+            textBoxDB.Text = settings.DefaultDB.EmptyCheck();
+            textBoxPassword.Text = settings.Password.EmptyCheck();
+            textBoxUserName.Text = settings.UserName.EmptyCheck();
+            textBoxServer.Text = settings.ServerName.EmptyCheck();
 
-            if (File.Exists(fileSettingsPath))                            
-                JsonConvert.DeserializeObject<AppSettings>(fileSettingsPath);
-
-            textBoxApiPath.Text = appSettings.APIPath.EmptyCheck();
-            textBoxDB.Text = appSettings.DefaultDB.EmptyCheck();
-            textBoxPassword.Text = appSettings.Password.EmptyCheck();
-            textBoxUserName.Text = appSettings.UserName.EmptyCheck();
-            textBoxServer.Text = appSettings.ServerName.EmptyCheck();
             
-
         }
 
 
         private void buttonOpenFolderDialog_Click(object sender, EventArgs e)
         {
             FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            
             if(folderBrowserDialog.ShowDialog() == DialogResult.OK)
-                appSettings.APIPath = textBoxApiPath.Text = folderBrowserDialog.SelectedPath;                
+                AppSettings.Instance.APIPath = textBoxApiPath.Text = folderBrowserDialog.SelectedPath;                
             
         }
 
         private void buttonSave_Click(object sender, EventArgs e)
         {
-            appSettings.APIPath = textBoxApiPath.Text;
-            appSettings.DefaultDB= textBoxDB.Text;
-            appSettings.Password= textBoxPassword.Text;
-            appSettings.UserName= textBoxUserName.Text;
-            appSettings.ServerName= textBoxServer.Text;
-
-            string output = JsonConvert.SerializeObject(appSettings);
-            using (StreamWriter sw = new StreamWriter(fileSettingsPath))
-            {
-                sw.Write(output);
-            }
+            var settings = AppSettings.Instance;
+            settings.APIPath = textBoxApiPath.Text;
+            settings.DefaultDB= textBoxDB.Text;
+            settings.Password= textBoxPassword.Text;
+            settings.UserName= textBoxUserName.Text;
+            settings.ServerName= textBoxServer.Text;
+            settings.Save();
+            
             Close();
         }
     }
