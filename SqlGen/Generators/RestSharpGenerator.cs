94085b37d04bca700326ff9cf05b61db3b08344e
Çağlar Dursun <caglardursun@yandex.com>
2020-04-17 12:17:08 +0300

    rest generator and screenshot added

---
+++ SqlGen/Generators/RestSharpGenerator.cs
@@ -0,0 +1,28 @@
+﻿using SqlGen.Templeates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlGen.Generators
+{
+    public class RestSharpGenerator : Generator
+    {
+        public override string Generate(Table table, GeneratorOptions options)
+        {
+
+            var restSharpClient = new RestSharpClient();
+
+            restSharpClient.Session = new Dictionary<string, object>();
+            restSharpClient.Session.Add("columns", table.InsertableColumns);
+            restSharpClient.Initialize();
+
+            return restSharpClient.TransformText();
+
+        }
+
+        public override string ToString() => "RestSharp Client Generator";
+        
+    }
+}
